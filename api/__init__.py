1249f8512560b1e3ce05248319ac3d4e1f2cac3e
agthompson175 <agthompson175@gmail.com>
2021-08-16 07:27:23 -0400

    cors fix

--- api/__init__.py
+++ api/__init__.py
@@ -6,7 +6,7 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    CORS(app, supports_credentials=True)
 
 
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
